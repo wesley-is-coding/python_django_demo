9a2d83fbbd9f36179305259e6e5897f89aa878b1
Wesley Campbell <icatch@gmail.com>
2015-11-03 11:45:24 -0300

    new URL, view and template to display lists

--- functional_test/tests.py
+++ functional_test/tests.py
@@ -84,7 +84,7 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-        
+
         # She visits that URL - her to-do list is still there.
 
-        # Satisfied, she goes back to sleep
+        # Satisfied, they both go back to sleep
